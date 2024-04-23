fa993f73b6a0b637c92a26ff0931e7a339e58442
Mario Martí <mariomartirubio21@gmail.com>
2024-04-23 19:30:14 +0200

    Creando repositorio de LEMA

---
+++ Java Script/Ejercicio1.js
@@ -0,0 +1,13 @@
+/*
+Todos los ejercicios del módulo deben estar en modo estricto ("use strict") y
+cumplir la nomeclatura de nombrado de archivos.
+Ejercicio 1.
+Crear una función que reciba una cadena y la devuelva transformada. Por ejemplo,
+si le pasamos a la función la cadena "una cadena como esta" obtendremos "UnA
+KaDeNa kOmO EsTaHHH". Para ello, hay que alternar el uso de MAYÚSCULAS y
+minúsculas, sustituir la letra C por la K y añadir tres letras H al final.
+La función se almacenará en una archivo denominado unidad4ejer01.js, y tendrá la
+siguiente definición:
+function toCani(cadena) {}*/
+
+"use strict";
---
+++ Java Script/Selectores.js
@@ -0,0 +1,35 @@
+//Ejercicio 1
+var todosLosParrafos = document.querySelectorAll("p");
+
+//Ejercicio 2
+var elementoConID = document.getElementById("spanElement");
+
+//Ejercicio 3
+var todosLosItemsDeLista = document.querySelectorAll("li");
+
+//Ejercicio 4
+var primerParrafo = document.querySelector("p");
+
+//Ejercicio 5
+var primerParrafo = document.querySelector("p");
+
+//Ejercicio 6
+
+//Ejercicio 7
+var parrafosHijosDirectos = document.getElementById("container").querySelectorAll(".paragraph");
+
+//Ejercicio 8
+var itemsDeLista = document.querySelectorAll("ul > li");
+
+//Ejercicio 9
+var parrafosDentroDeContainer = document.getElementById("container").querySelectorAll(".paragraph");
+
+//Ejercicio 10
+var itemsDeListaUl = document.querySelectorAll("ul li");
+
+//Ejercicio 11
+var primerParrafoEnContainer = document.getElementById("container").querySelector("p");
+
+//Ejercicio 12
+var elementosHijosDelDocumento = document.body.children;
+
